9d2da6f02419603d372fb6e7836e3d9622727fef
Prapatjain <prapatjain@gmail.com>
2025-03-08 22:33:15 +0530

    handling rest api's using service layer without db

--- src/main/java/com/example/Address/Book/controllers/AddressController.java
+++ src/main/java/com/example/Address/Book/controllers/AddressController.java
@@ -3,12 +3,16 @@ package com.example.Address.Book.controllers;
 import java.util.*;
 import com.example.Address.Book.dto.EmployeeDTO;
 import com.example.Address.Book.dto.ResponseDTO;
+import com.example.Address.Book.interfaces.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addressbook")
 public class AddressController {
 
+    @Autowired
+    IEmployeeService iEmployeeService;
 
     //UC1 --> REST API's handled using responseDTO(without interference of service layer)
 
@@ -37,7 +41,32 @@ public class AddressController {
         return new ResponseDTO("API triggered at /res/delete/{id}", "Success");
     }
 
+    //UC2 --> Handling REST API's using Service layer without storing in DB
 
+    @GetMapping("/res2/get/{id}")
+    public ResponseDTO get2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/get/{id}", "Success");
+    }
+
+    @PostMapping("/res2/create")
+    public ResponseDTO create2(@RequestBody EmployeeDTO user){
+        return iEmployeeService.response("API triggered at /res/create", "Success");
+    }
+
+    @GetMapping("/res2/getAll")
+    public ResponseDTO getAll2(){
+        return iEmployeeService.response("API triggered at /res/getAll", "Success");
+    }
+
+    @PutMapping("/res2/edit/{id}")
+    public ResponseDTO edit2(@RequestBody EmployeeDTO user, @PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/edit/{id}", "Success");
+    }
+
+    @DeleteMapping("/res2/delete/{id}")
+    public ResponseDTO delete2(@PathVariable Long id){
+        return iEmployeeService.response("API triggered at /res/delete/{id}", "Success");
+    }
 
 
 
